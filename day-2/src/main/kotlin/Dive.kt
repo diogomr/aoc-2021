2cc675748945697d82bb13445eec53465d69d975
Diogo Moreira <diogo.moreira@talkdesk.com>
2021-12-02 09:34:23 +0000

    Day Two Solution

---
+++ day-2/src/main/kotlin/Dive.kt
@@ -0,0 +1,65 @@
+fun main() {
+
+    partOne().apply {
+        println("Part One Solution: $this")
+    }
+
+    partTwo().apply {
+        println("Part Two Solution: $this")
+    }
+}
+
+private fun partOne(): Int {
+    var horizontal = 0
+    var depth = 0
+
+    readInput()
+        .map {
+            when (it.first) {
+                Command.FORWARD -> horizontal += it.second
+                Command.DOWN -> depth += it.second
+                Command.UP -> depth -= it.second
+            }
+        }
+
+    return horizontal * depth
+}
+
+private fun partTwo(): Int {
+    var aim = 0
+    var horizontal = 0
+    var depth = 0
+
+    readInput()
+        .map {
+            when (it.first) {
+                Command.FORWARD -> {
+                    horizontal += it.second
+                    depth += aim * it.second
+                }
+                Command.DOWN -> aim += it.second
+                Command.UP -> aim -= it.second
+            }
+        }
+
+    return horizontal * depth
+}
+
+private fun readInput(): List<Pair<Command, Int>> {
+    return {}::class.java.classLoader.getResource("input.txt")
+        .readText()
+        .split("\n")
+        .filter { it.isNotBlank() }
+        .map {
+            val split = it.split(" ")
+            val command = Command.valueOf(split[0].uppercase())
+            val value = split[1].toInt()
+            Pair(command, value)
+        }
+}
+
+enum class Command {
+    FORWARD,
+    DOWN,
+    UP,
+}
